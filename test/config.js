19a7e057b9a5ac499a3e1f34c3a624132a27c781
Dan Macumber <dan.macumber@gmail.com>
2024-06-11 10:58:07 -0600

    Add firebase test

---
+++ test/config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  // Your web app's Firebase configuration here
+  // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
+  apiKey: "AIzaSyCWKeYYj4JL-998jUeUDAShPluEmvCqrNg",
+  authDomain: "testproject-c3dda.firebaseapp.com",
+  projectId: "testproject-c3dda",
+  storageBucket: "testproject-c3dda.appspot.com",
+  messagingSenderId: "324206416101",
+  appId: "1:324206416101:web:7642a70ecc4af7cf54440c",
+  measurementId: "G-GD1G371C2J"
+};
